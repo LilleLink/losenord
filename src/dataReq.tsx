74f01fa005ef0ff3c4541c57b8f70db287a77350
Cheddaren <GitHub@ontheline.nu>
2021-07-15 16:27:54 +0200

    Updateing requests

--- src/dataReq.tsx
+++ src/dataReq.tsx
@@ -16,3 +16,23 @@ export function getReq() {
       .then(data => console.log(data))
       .catch(error => console.log('error'))
 }
+
+export function postReq() {
+  fetch('https://cors.dialekt.it/https://backendpw.dialekt.it/p.json', {
+    method: 'POST',
+    headers: { 'Accept': 'application/json'},
+
+  },
+  body: JSON.stringify({payload: 'Hej',})
+  )
+    .then(res => {
+      if(res.ok) {
+        console.log('SUCCESSFUL')
+      }
+      else {
+        console.log('Failed')
+      }
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('error'))
+}
