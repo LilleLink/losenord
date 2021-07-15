7b69b251a4ab7aea6858b72c69ff05419046242a
Cheddaren <GitHub@ontheline.nu>
2021-07-15 21:55:50 +0200

    Updated POST Request (Broken atm)

--- src/dataReq.tsx
+++ src/dataReq.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, } from 'react';
 
 export function getReq() {
   fetch('https://cors.dialekt.it/https://backendpw.dialekt.it/p/xtwcdmvuhgx7t7r1.json', {
@@ -17,13 +17,30 @@ export function getReq() {
       .catch(error => console.log('error'))
 }
 
-export function postReq(password, days, uses,) {
-  fetch('https://cors.dialekt.it/https://backendpw.dialekt.it/p.json', {
+export function postReq(payload: string, expire_after_days: number , expire_after_views: number,) {
+
+  const [postId, setPostId] = useState(null);
+
+  useEffect(() => {
+    const ReqOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({payload: payload, expire_after_days: expire_after_days, expire_after_views: expire_after_views}),
+    };
+    fetch('https://cors.dialekt.it/https://backendpw.dialekt.it/p.json', ReqOptions)
+      .then(response => response.json())
+      .then(data => setPostId(data.id));
+  }, []);
+  return{
+    postId
+  }
+
+/*  fetch('https://cors.dialekt.it/https://backendpw.dialekt.it/p.json', {
     method: 'POST',
     headers: { 'Accept': 'application/json'},
 
   },
-  body: JSON.stringify({payload: 'Hej',})
+  body: JSON.stringify({payload: payload, expire_after_days, expire_after_views})
   )
     .then(res => {
       if(res.ok) {
@@ -35,4 +52,6 @@ export function postReq(password, days, uses,) {
     })
     .then(data => console.log(data))
     .catch(error => console.log('error'))
+}
+*/
 }
